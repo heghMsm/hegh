998ed19ed90534d7731715f3856917342918f7f7
heghMsm <13755201404@163.com>
2019-12-25 14:26:54 +0800

    完善登录接口

--- leyou/leyou-auth/leyou-auth-service/src/main/java/com/leyou/auth/controller/AuthController.java
+++ leyou/leyou-auth/leyou-auth-service/src/main/java/com/leyou/auth/controller/AuthController.java
@@ -2,19 +2,16 @@ package com.leyou.auth.controller;
 
 import com.leyou.auth.config.JwtConfig;
 import com.leyou.auth.service.AuthService;
+import com.leyou.common.dto.UserInfoDto;
 import com.leyou.common.enums.ExceptionEnum;
 import com.leyou.common.utils.CookieUtils;
 import com.leyou.common.utils.Result;
 import com.leyou.common.utils.ResultUtil;
-import com.netflix.ribbon.proxy.annotation.Http;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,19 +30,37 @@ public class AuthController {
     @Autowired
     private JwtConfig jwtConfig;
 
-    @PostMapping("accredit")
+    /**
+     * 用户登录接口
+     * @param username
+     * @param password
+     * @param request
+     * @param response
+     * @return
+     */
+    @PostMapping("login")
     public Result accredit(String username, String password, HttpServletRequest request, HttpServletResponse response){
         if (StringUtils.isBlank(username) || StringUtils.isBlank(password)){
             ResultUtil.fail(ExceptionEnum.CREATE_TOKEN_ERROR);
             logger.info("用户名或密码为空");
         }
-        String token = authService.accredit(username,password);
-        if (StringUtils.isBlank(token)){
-            logger.info(username+" 生成token失败");
-            ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        UserInfoDto userInfoDto = null;
+        try {
+            userInfoDto = authService.login(username,password);
+        } catch (Exception e) {
+            logger.info("登录错误");
+            e.printStackTrace();
         }
-        CookieUtils.setCookie(request,response,jwtConfig.getCookieName(),token,jwtConfig.getExpire() * 60);
-        return ResultUtil.succ(token);
+        if (userInfoDto == null){
+            logger.info("用户名或密码错误");
+            return ResultUtil.fail(ExceptionEnum.INVALID_USERNAME_PASSWORD);
+        }
+        if (StringUtils.isBlank(userInfoDto.getToken())){
+            logger.info(username+" 生成token失败");
+            ResultUtil.fail(ExceptionEnum.CREATE_TOKEN_ERROR);
+        }
+        CookieUtils.setCookie(request,response,jwtConfig.getCookieName(),userInfoDto.getToken(),jwtConfig.getExpire() * 60);
+        return ResultUtil.succ(userInfoDto);
 
     }
 }
--- leyou/leyou-auth/leyou-auth-service/src/main/java/com/leyou/auth/service/AuthService.java
+++ leyou/leyou-auth/leyou-auth-service/src/main/java/com/leyou/auth/service/AuthService.java
@@ -2,16 +2,24 @@ package com.leyou.auth.service;
 
 import com.leyou.auth.client.UserClient;
 import com.leyou.auth.config.JwtConfig;
+import com.leyou.common.dto.UserInfoDto;
 import com.leyou.common.pojo.UserInfo;
 import com.leyou.common.utils.JwtUtils;
+import com.leyou.common.utils.RedisClient;
 import com.leyou.user.pojo.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 @EnableConfigurationProperties(JwtConfig.class)
 public class AuthService {
+
+    private Logger logger = LoggerFactory.getLogger(getClass().getName());
 
     @Autowired
     private UserClient userClient;
@@ -19,22 +27,34 @@ public class AuthService {
     @Autowired
     private JwtConfig jwtConfig;
 
-    public String accredit(String username, String password) {
-        //根据用户名和密码查询
+    @Autowired
+    private RedisClient redisClient;
+
+    /**
+     * 用户登录
+     * @param username
+     * @param password
+     * @return
+     * @throws Exception
+     */
+    public UserInfoDto login(String username, String password) throws Exception {
+        UserInfoDto userInfoDto = new UserInfoDto();
         User user = userClient.queryUser(username, password);
         if (user == null){
+            logger.info("用户"+username+"不存在");
             return null;
         }
-        //生成token
         UserInfo userInfo = new UserInfo();
         userInfo.setId(user.getId());
         userInfo.setUsername(user.getUsername());
-        try {
-           return JwtUtils.generateToken(userInfo,jwtConfig.getPriKey(),jwtConfig.getExpire());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String token = JwtUtils.generateToken(userInfo,jwtConfig.getPriKey(),jwtConfig.getExpire());
+        redisClient.set("token_" + user.getId().toString(),token,30, TimeUnit.DAYS);
+        userInfoDto.setUserId(user.getId());
+        userInfoDto.setUsername(user.getUsername());
+        userInfoDto.setPhone(user.getPhone());
+        userInfoDto.setCreated(user.getCreated());
+        userInfoDto.setToken(token);
 
-        return null;
+        return userInfoDto;
     }
 }
